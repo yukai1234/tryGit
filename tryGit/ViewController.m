7cdae882d6062a1ac428c33749ed33de22e8faf6
俞凯 <ykymy@192.168.3.148>
2016-04-18 13:52:09 +0800

    llog

--- tryGit/ViewController.m
+++ tryGit/ViewController.m
@@ -18,6 +18,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"ooooo");
+    NSLog(@"ooooo");
+
 }
 
 - (void)didReceiveMemoryWarning {
