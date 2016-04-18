da644aaba15ac52cd6620bae096fae0092dc0ddd
俞凯 <ykymy@192.168.3.148>
2016-04-18 13:44:44 +0800

    Initial Commit

---
+++ tryGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tryGit
+//
+//  Created by 俞凯 on 16/4/18.
+//  Copyright © 2016年 俞凯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tryGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tryGit
+//
+//  Created by 俞凯 on 16/4/18.
+//  Copyright © 2016年 俞凯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
